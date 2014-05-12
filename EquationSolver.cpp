eba1407b78f080c52fa9b9f97c355408742e2b20
Hsu, Tien-Yiao <meteorologytoday@gmail.com>
2014-05-12 11:23:27 +0800

    Creating EquationSolver.cpp

---
+++ EquationSolver.cpp
@@ -0,0 +1,11 @@
+#ifndef XLABEE_EQUATION_SOLVER_CPP
+#define XLABEE_EQUATION_SOLVER_CPP
+
+namespace XLABEE {
+	template <typename T, x_size Nr, x_size Nz>
+    	EESolver<T, Nr, Nz>::EESolver() {
+
+    };
+}
+
+#endif
